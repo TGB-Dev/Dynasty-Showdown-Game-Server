baf11a5e6e8d404caca4b6d7e3e5ad23ebaeedff
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2025-03-24 22:35:56 +0700

    deps: installed Socket.io and Mongoose adapter

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  endOfLine: 'lf',
+};
+
+export default config;
