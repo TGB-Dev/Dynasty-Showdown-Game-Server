7bc7e5a3a0c3f3282cc8efd810e5e1c9693ec5d2
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2025-03-25 08:50:41 +0700

    feat: added TestGateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017')],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017'), TestModule],
   controllers: [AppController],
   providers: [AppService],
 })
