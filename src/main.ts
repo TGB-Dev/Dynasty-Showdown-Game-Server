e6627e26b4f534c0faac700b052947bfaef3daf9
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2025-03-25 08:34:04 +0700

    deps: installed Swagger, Socket.io and Mongoose adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configs
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Dynasty Showdown Game Server Documentation')
+    .setVersion('1.0')
+    .setDescription('The documentation of the Dynasty Showdown Game Server')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
