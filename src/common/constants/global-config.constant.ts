c93fa061a67e92f335924d886fecd61e4c82e6e5
NhanElastic <nvo300108@gmail.com>
2025-03-31 23:56:26 +0700

    update env

--- src/common/constants/global-config.constant.ts
+++ src/common/constants/global-config.constant.ts
@@ -1,5 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 export const globalConfigs = {
   mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/dynasty-showdown',
   serverPort: process.env.SERVER_PORT || 5001,
   jwtSecret: process.env.JWT_SECRET || 'TGBDynastyShowdownGameServerJWTSecret',
 };
+g
