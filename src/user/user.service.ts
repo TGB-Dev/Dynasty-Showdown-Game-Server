70f43afc3f1c5ca92574352fdc00315a2fb1e237
Binh Minh <bmmit.work@gmail.com>
2025-04-02 13:15:54 +0700

    feat(score): Add update score endpoint and service for user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { User } from '../schemas/user.schema';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
+
+  async updateScore(user_id: string, action: 'set' | 'inc', score: number) {
+    let result: User | null;
+
+    if (action === 'set') {
+      result = await this.userRepository.setScore(user_id, score);
+    } else {
+      result = await this.userRepository.increaseScore(user_id, score);
+    }
+
+    if (result === null) throw new NotFoundException();
+    return result;
+  }
 }
