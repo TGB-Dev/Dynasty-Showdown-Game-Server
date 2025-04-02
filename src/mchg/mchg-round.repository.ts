d3eab99117a54bfaffa9c20d6800daa986dfbc6b
Binh Minh <bmmit.work@gmail.com>
2025-04-02 10:51:05 +0700

    feat(mchg-rounds): rename repository

--- src/mchg/mchg-round.repository.ts
+++ src/mchg/mchg-round.repository.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { MchgRound } from '../schemas/mchg/mchgRound.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 
 @Injectable()
 export class MchgRepository {
   constructor(@InjectModel(MchgRound.name) private readonly mchgRoundModel: Model<MchgRound>) {}
 
-  createRound(roundDto: any): Promise<MchgRound> {
+  create(roundDto: any): Promise<MchgRound> {
     const newRound = new this.mchgRoundModel(roundDto);
     return newRound.save();
+  }
+
+  getAll(): Promise<MchgRound[]> {
+    return this.mchgRoundModel.find().exec();
+  }
+
+  async getByOrder(order: number): Promise<MchgRound> {
+    return (await this.mchgRoundModel.find().sort({ order: 1 }).exec())[order];
   }
 }
